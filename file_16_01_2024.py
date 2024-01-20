57e3ff8bd157c7499b94e0c749a7377dd84f40bd
SkChangthu <156789281+SkChangthu@users.noreply.github.com>
2024-01-20 22:34:38 +0530

    Add files via upload

---
+++ file_16_01_2024.py
@@ -0,0 +1,76 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print('hello world')
+hello world
+print('5*6')
+5*6
+print(6*5)
+30
+print('sk'*5)
+sksksksksk
+print('sahil'+3)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print('sahil'+3)
+TypeError: can only concatenate str (not "int") to str
+print('Sahil+3')
+Sahil+3
+print('Sahil'+'3')
+Sahil3
+print(type(1.5))
+<class 'float'>
+x=1.5
+type(x)
+<class 'float'>
+type(y)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    type(y)
+NameError: name 'y' is not defined
+y=1.5
+type(y)
+<class 'float'>
+x+y
+3.0
+print(x+y)
+3.0
+>>> PRINT('193')
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    PRINT('193')
+NameError: name 'PRINT' is not defined
+>>> SKC=33
+>>> TYPE('SKC')
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    TYPE('SKC')
+NameError: name 'TYPE' is not defined
+>>> type('skc')
+<class 'str'>
+>>> type(skc)
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    type(skc)
+NameError: name 'skc' is not defined. Did you mean: 'SKC'?
+>>> type(SKC)
+<class 'int'>
+>>> SK=30
+>>> type(SK)
+<class 'int'>
+>>> SKC+SK
+63
+>>> SKC*SK
+990
+>>> SKC/SK
+1.1
+>>> sahil=60
+>>> sahin=30
+>>> result=sahil/sahin
+>>> result
+2.0
+>>> type(sahil)
+<class 'int'>
+>>> type(sahin)
+<class 'int'>
+>>> type(result)
+<class 'float'>
